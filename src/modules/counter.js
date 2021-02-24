a39c5427ea7a05db8c14277689cb1ef4d0ae49c7
wyjeon <wyjeon.info@gmail.com>
2021-02-24 13:23:38 +0900

    redux-action 리팩토링

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,12 +1,13 @@
 //Ducks 패턴을 사용하여 작성
+import { createAction, handleActions } from 'redux-actions';
 
 // 액션 타입 정의
 const INCREASE = 'counter/INCREASE'; // "모듈 이름/액션 이름" 형식으로 작성
 const DECREASE = 'counter/DECREASE';
 
 // 액션 생성 함수 만들기
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 // 초기 상태
 const initialState = {
@@ -14,13 +15,12 @@ const initialState = {
 };
 
 // 리듀서 함수 만들기
-export default function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return { number: state.number + 1 };
-    case DECREASE:
-      return { number: state.number - 1 };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
+
+export default counter;
